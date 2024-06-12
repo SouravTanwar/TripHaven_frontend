5fb36ad7cef24560107b7e887068cc999177e1cf
Sourav Tanwar <souravtanwar30@gmail.com>
2024-06-12 11:36:44 +0530

    Fix eslint warning

--- src/components/HotelImages/HotelImages.jsx
+++ src/components/HotelImages/HotelImages.jsx
@@ -8,11 +8,11 @@ export const HotelImages = ({singleHotel}) => {
 
         <div className="hotel-image-container d-flex gap-small">
             <div className="primary-image-container">
-                <img className="primary-img" src = {image} alt="primary-image" />
+                <img className="primary-img" src = {image} alt="Primary view of the hotel" />
             </div>
             <div className="d-flex wrap gap-small">
                 {
-                    imageArr && imageArr.map(image => <img className="hotel-img" src= {image} alt="hotel-image" /> )
+                    imageArr && imageArr.map(image => <img className="hotel-img" src= {image} alt="hotel-view" /> )
                 }
             </div>
         </div>
